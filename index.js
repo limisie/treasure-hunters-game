1fbed88312cac26620485814e3a5e4008350659d
limisie <k.limisiewicz@gmail.com>
2022-05-30 23:04:41 +0200

    fix: render canvas

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import Player from './Components/player.js';
 import Platform from './Components/platform.js';
-import {c, canvas} from './Components/canvas.js';
 import {move, keyUp, keyInput} from './Components/movementController.js';
+
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const player = new Player(canvas);
 const platforms = [new Platform(canvas), new Platform(canvas, 500, 600), new Platform(canvas, 900, 900)];
+
 
 function endGame() {
     console.log('You Won!');
 }
 
+
 function run() {
+    if (player.scrollOffset < 1000) {
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        requestAnimationFrame(run);
+    } else {
+        endGame();
+    }
+    
     move(player, platforms);
     
     platforms.map(p => {
@@ -19,16 +33,10 @@ function run() {
             player.velocity.y = 0;
         }
     });
-    
-    if (player.scrollOffset >= 1000) {
-        endGame();
-    } else {
-        requestAnimationFrame(run);
-        c.clearRect(0, 0, canvas.width, canvas.height);
-    }
 }
+
 
 run();
 
-window.addEventListener('keydown', ({keyCode, repeat}) => keyInput(keyCode, repeat));
+window.addEventListener('keydown', ({keyCode}) => keyInput(keyCode));
 window.addEventListener('keyup', ({keyCode}) => keyUp(keyCode));
