a0c7e73e2e210875468cf65a4995ee01ecaf89c0
limisie <k.limisiewicz@gmail.com>
2022-05-30 19:30:34 +0200

    feat: player input

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import Player from './player.js';
 import {c, canvas} from './canvas.js';
+import {playerMovement, keyUp, keyInput} from './controller.js';
 
 const player = new Player(canvas);
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
-    player.update();
+    playerMovement(player);
 }
 
 animate();
+
+window.addEventListener('keydown', ({keyCode}) => keyInput(keyCode));
+window.addEventListener('keyup', ({keyCode}) => keyUp(keyCode));
--- player.js
+++ player.js
@@ -1,19 +1,26 @@
-const gravity = 0.5;
+const GRAVITY = 0.8;
+
+const INITIAL_POSITION_X = 100;
+const INITIAL_POSITION_Y = 100;
+const INITIAL_WIDTH_Y = 30;
+const INITIAL_HEIGHT_Y = 30;
+const MAX_VELOCITY = 10;
 
 class Player {
     constructor(canvas) {
+        this.maxVelocity = MAX_VELOCITY;
         this.canvas = canvas;
         this.context = canvas.getContext('2d');
         
         this.position = {
-            x: 100, y: 100
+            x: INITIAL_POSITION_X, y: INITIAL_POSITION_Y
         };
         this.velocity = {
             x: 0, y: 0
         };
         
-        this.width = 30;
-        this.height = 30;
+        this.width = INITIAL_WIDTH_Y;
+        this.height = INITIAL_HEIGHT_Y;
     }
     
     draw() {
@@ -23,13 +30,24 @@ class Player {
     
     update() {
         this.draw();
+        
         this.position.y += this.velocity.y;
+        this.position.x += this.velocity.x;
         
         if (this.position.y + this.height + this.velocity.y <= this.canvas.height) {
-            this.velocity.y += gravity;
+            this.velocity.y += GRAVITY;
         } else {
             this.velocity.y = 0;
         }
+    }
+    
+    addVelocity(x, y) {
+        if (this.velocity.x < MAX_VELOCITY && this.velocity.x > -MAX_VELOCITY) this.velocity.x += x;
+        if (this.velocity.y < MAX_VELOCITY && this.velocity.y > -MAX_VELOCITY) this.velocity.y += y;
+    }
+    
+    stop() {
+        this.velocity.x = 0;
     }
 }
 
